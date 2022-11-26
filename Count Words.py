38ac0cf26acc10272e5858cf6f6cdecc336b0a1a
Derragui <79793586+Derragui@users.noreply.github.com>
2022-11-26 11:47:46 +0100

    Update Count Words.py

--- Count Words.py
+++ Count Words.py
@@ -1,5 +1,7 @@
 import string
 i=0
+list=[]
+num=[]
 #i had an issue with my file my python couldn't find it even tho both where in the same folder so i gave the full route
 with open("C:\\Users\Dell\Documents\my_txt_file.txt", "r") as f:
     data=f.read()
@@ -9,5 +11,17 @@ with open("C:\\Users\Dell\Documents\my_txt_file.txt", "r") as f:
     for word in lines:
         #not obliged to add this if
         #if not word.isnumeric():
+        if not(word in list):
+            list.append(word)
+            num.append(1)
+        else:
+            num[list.index(word)]+=1
         i+=1
+print('-------------------------------------------')
+for x in range(len(list)):
+    print(f'Word "{list[x]}"')
+    print(f'Number of itteration :{num[x]}')
+    print("*****")
+    
+print('-------------------------------------------')
 print(f'The number of words in this text file is: {i}')
