04250972a28e6b66fc3c71b9a99bff4f7cc346fb
Derragui <79793586+Derragui@users.noreply.github.com>
2022-12-22 11:52:20 +0100

    update

--- py test/Stock Data.py
+++ py test/Stock Data.py
@@ -1,5 +1,5 @@
-my_file = 'C:/Users/Dell/Documents/py test/GOOG2.csv'
-
+my_file = 'C:/Users/Dell/Documents/GitHub/Python-programing/py test/GOOG2.csv'
+#my_file = input("enter path :")
 m = []
 with open(my_file, 'r') as f:
     i = 0
@@ -8,26 +8,27 @@ with open(my_file, 'r') as f:
         i += 1
         if(i==1):
             continue
-        print(f'{i}: {line}', end='')
         for c in line.split(','):
-            print(c)
+            c=c.strip()
             r.append(c)
             #print(type(c))
         m.append(r)
-print()
 
-
-new_file="C:/Users/Dell/Documents/py test/new.csv"
+new_file="C:/Users/Dell/Documents/GitHub/Python-programing/py test/new.csv"
 # 'a' - append to end of file
 with open(new_file, 'w') as f: 
-    change=[str((float(r[4])-float(r[1]))/float(r[1])) for r in m]
+    change=[str(((float(r[4])-float(r[1]))/float(r[1]))*100) for r in m]
+    listc=[]
+    listc.append(["Date ","Open ","High ","Low ","Close ","Adj " "Close "," Volume"])
     for i in range(len(m)):
         m[i].append(change[i])
-    for r in m:
+        listc.append(m[i])
+        
+    for r in listc:
         print(r)
-        cells = [str(i) for i in r]
-        print(cells)
-        f.write('\t'.join(cells)+'\n')
+    for r in listc:
+        cells = [i for i in r]
+        f.write('\t,'.join(cells)+'\n')
 
     
   
